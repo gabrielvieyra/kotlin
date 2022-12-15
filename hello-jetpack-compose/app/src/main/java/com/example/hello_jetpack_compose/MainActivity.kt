1d87b50e7b6c95f812a226243df86ab8ee524e0f
Gabriel Vieyra <vieyra.gabriel08@gmail.com>
2022-12-15 17:08:05 -0300

    probando modificadores

--- hello-jetpack-compose/app/src/main/java/com/example/hello_jetpack_compose/MainActivity.kt
+++ hello-jetpack-compose/app/src/main/java/com/example/hello_jetpack_compose/MainActivity.kt
@@ -3,31 +3,60 @@ package com.example.hello_jetpack_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.hello_jetpack_compose.ui.theme.HellojetpackcomposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         /* setContent, este bloque de codigo es nuestra activity, todo lo que coloquemos aca adentro se va a pintar en la pantalla de nuestra app */
         setContent {
-            PersonalData(name = "Gabriel");
+            HellojetpackcomposeTheme() {
+                MyComponent();
+            }
         }
     }
 
-    /* Creamos una funcion privada que va a renderizar un texto */
+    /* Me permite ver como van quedando los cambios en tiempo real */
+    @Preview
     @Composable
+    fun MyComponent() {
+        // Los modificadores me permiten cambiar los estilos por default que tienen los componentes
+        Row(modifier = Modifier.padding(8.dp).background(Color.LightGray)) {
+            MyImage();
+            MyTexts();
+        }
+    }
+
+    @Composable
+    fun MyImage() {
+        Image(
+            painter = painterResource(id = R.drawable.avatar_messi),
+            contentDescription = "Messi avatar",
+            modifier = Modifier.width(200.dp)
+                )
+    }
+
+    @Composable
+    fun MyTexts() {
+        Column() {
+            Text(text = "¡Hola Jetpack Compose!");
+            Text(text = "¿Preparado?");
+        }
+    }
+
+    // Primeras pruebas
+    /* Creamos una funcion privada que va a renderizar un texto */
+    /*@Composable
     private fun PersonalData(name: String) {
         // Los elementos que esten adentro de MaterialTheme se van a ver afectados
         MaterialTheme() {
@@ -47,14 +76,7 @@ class MainActivity : ComponentActivity() {
                 Text(text = "test@gmail.com");
             }
         }
-    }
-
-    /* Me permite visualizar como se ve la ui en tiempo real */
-    @Preview
-    @Composable
-    fun PreviewPersonalData() {
-        PersonalData(name = "Gabriel");
-    }
+    }*/
 }
 
 
