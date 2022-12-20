8690ae7a9763dfa085b3914f02622e0ea604e40f
Gabriel Vieyra <vieyra.gabriel08@gmail.com>
2022-12-20 10:26:00 -0300

    agrego proyecto jetpack-compose-navigation

--- hello-jetpack-compose/app/src/main/java/com/example/hello_jetpack_compose/MainActivity.kt
+++ hello-jetpack-compose/app/src/main/java/com/example/hello_jetpack_compose/MainActivity.kt
@@ -3,37 +3,55 @@ package com.example.hello_jetpack_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.hello_jetpack_compose.ui.theme.HellojetpackcomposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         /* setContent, este bloque de codigo es nuestra activity, todo lo que coloquemos aca adentro se va a pintar en la pantalla de nuestra app */
         setContent {
-            HellojetpackcomposeTheme() {
-                MyComponent();
-            }
+            PreviewComponent();
+        }
+    }
+
+    @Preview(showSystemUi = true)
+    @Composable
+    fun PreviewComponent() {
+        val scrollState = rememberScrollState();
+
+        Column(modifier = Modifier.verticalScroll(scrollState)) {
+            MyComponent();
+            MyComponent();
+            /*MyComponent();
+            MyComponent();
+            MyComponent();
+            MyComponent();*/
         }
     }
 
     /* Me permite ver como van quedando los cambios en tiempo real */
-    @Preview
+    // showSystemUi me permite ver el preview en el emulador
+    //@Preview()
     @Composable
     fun MyComponent() {
         // Los modificadores me permiten cambiar los estilos por default que tienen los componentes
-        Row(modifier = Modifier.padding(8.dp).background(Color.LightGray)) {
+        Row(modifier = Modifier
+            .background(Color.LightGray)
+            .fillMaxWidth()) {
             MyImage();
-            MyTexts();
+            MyTexts(title = "¡Hola Jetpack Compose!", body = "¿Preparado? Probando texto largo Probando texto largo");
         }
     }
 
@@ -42,15 +60,20 @@ class MainActivity : ComponentActivity() {
         Image(
             painter = painterResource(id = R.drawable.avatar_messi),
             contentDescription = "Messi avatar",
-            modifier = Modifier.width(200.dp)
+            modifier = Modifier.width(200.dp),
                 )
     }
 
     @Composable
-    fun MyTexts() {
-        Column() {
-            Text(text = "¡Hola Jetpack Compose!");
-            Text(text = "¿Preparado?");
+    fun MyTexts(title: String, body: String) {
+        // Me permite definir el valor del estado inicial
+        var isExpanded by remember { mutableStateOf( value = false) }
+
+        Column(modifier = Modifier.padding(8.dp).clickable {
+            isExpanded = !isExpanded;
+        }) {
+            Text(text = "$title", maxLines = if (isExpanded) Int.MAX_VALUE else 1)
+            Text(text = "$body", maxLines = if (isExpanded) Int.MAX_VALUE else 1)
         }
     }
 
