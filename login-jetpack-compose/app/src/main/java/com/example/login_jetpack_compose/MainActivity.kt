894c0d64816aa86f2452cf8ac921e6f50961bcec
Gabriel Vieyra <vieyra.gabriel08@gmail.com>
2022-12-22 16:04:09 -0300

    agrego pantalla de login

--- login-jetpack-compose/app/src/main/java/com/example/login_jetpack_compose/MainActivity.kt
+++ login-jetpack-compose/app/src/main/java/com/example/login_jetpack_compose/MainActivity.kt
@@ -6,38 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.login_jetpack_compose.ui.theme.LoginjetpackcomposeTheme
+import com.example.login_jetpack_compose.view.LoginScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LoginjetpackcomposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            LoginScreen()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    LoginjetpackcomposeTheme {
-        Greeting("Android")
     }
 }
--- login-jetpack-compose/app/src/main/java/com/example/login_jetpack_compose/view/LoginScreen.kt
+++ login-jetpack-compose/app/src/main/java/com/example/login_jetpack_compose/view/LoginScreen.kt
@@ -1,4 +1,139 @@
 package com.example.login_jetpack_compose.view
 
-class LoginScreen {
+import android.content.ContentValues.TAG
+import android.nfc.Tag
+import android.util.Log
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LoginScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Login Screen")
+                }
+            )
+        }
+    ) {
+        val padding = 24.dp
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(padding),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Bienvenido",
+                style = MaterialTheme.typography.h6
+                )
+            Spacer(Modifier.height(32.dp))
+            TextField(
+                value = "",
+                modifier = Modifier.fillMaxWidth(),
+                label = {
+                    Text(text = "Usuario")
+                },
+                onValueChange = {}
+            )
+            Spacer(Modifier.height(24.dp))
+            TextField(
+                value = "",
+                modifier = Modifier.fillMaxWidth(),
+                label = {
+                    Text(text = "Clave de identificacion")
+                },
+                onValueChange = {}
+            )
+
+            Spacer(Modifier.height(24.dp))
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {
+                    Log.d(TAG, "El boton fue apretado")
+                }
+            ) {
+                Text(text = "Ingresar")
+            }
+
+            Spacer(Modifier.height(24.dp))
+            Text(
+                text = "Recuperar usuario"
+            )
+            Spacer(Modifier.height(12.dp))
+            Text(
+                text = "Recuperar clave de identificacion"
+            )
+
+            Spacer(Modifier.height(24.dp))
+            Text(
+                text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras luctus pharetra " +
+                        "ante, eu bibendum eros. Nullam lorem massa, ullamcorper sit amet lorem id, " +
+                        "iaculis semper eros. Mauris fermentum elementum lectus quis laoreet. Praesent " +
+                        "sodales maximus massa et sodales. Suspendisse potenti.",
+                color = Color.Gray,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
 }
+
+@Preview
+@Composable
+fun Preview() {
+    LoginScreen()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
