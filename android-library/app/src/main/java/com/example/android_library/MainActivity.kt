0c395d08e8331eefd7b508834931958a6caf17bf
Gabriel Vieyra <vieyra.gabriel08@gmail.com>
2022-12-29 17:43:57 -0300

    agrego nueva libreria

--- android-library/app/src/main/java/com/example/android_library/MainActivity.kt
+++ android-library/app/src/main/java/com/example/android_library/MainActivity.kt
@@ -3,33 +3,29 @@ package com.example.android_library
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.android_library.ui.theme.AndroidlibraryTheme
+import com.example.mylibrary.ButtonPreview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AndroidlibraryTheme {
-                test()
+                Box(modifier = Modifier.fillMaxSize()) {
+                    ButtonPreview()
+                }
             }
         }
     }
 }
 
-@Composable
-fun test() {
-    Text(text = "Hello world")
-}
-
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
-    test()
+    ButtonPreview()
 }
---
+++ android-library/mylibrary/src/main/java/com/example/mylibrary/Button.kt
@@ -0,0 +1,17 @@
+package com.example.mylibrary
+
+import android.content.ContentValues.TAG
+import android.util.Log
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ButtonPreview() {
+    Text(
+        text = "Probando libreria",
+        style = MaterialTheme.typography.h6
+    )
+}
